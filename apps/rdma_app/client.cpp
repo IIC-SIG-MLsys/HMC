0e277f562fd68d5d0d15bac9cc0d8a6299365863
072020127 <mhy200253@gmail.com>
2025-05-28 17:15:09 +0800

    speed test

--- apps/rdma_app/client.cpp
+++ apps/rdma_app/client.cpp
@@ -1,115 +1,153 @@
 #include <chrono>
 #include <glog/logging.h>
-#include <hmc.h>
 #include <iostream>
+#include <thread>
 #include <cmath>
+#include <fstream>
+#include <unistd.h>
 #include <vector>
-#include <future>
+#include <cmath>
+#include <iomanip>
+#include <hmc.h>
 
 using namespace hmc;
+using namespace std;
+using namespace std::chrono;
 
-struct ChannelContext {
-  int id;
-  Communicator* comm;
-  void* data_ptr;
-  size_t size;
-};
+const int base_port = 2025;
+const int device_id = 1;
+const size_t buffer_size = 2048ULL * 1024 * 16;
 
-void send_channel_slice(ChannelContext ctx) {
-  auto status = ctx.comm->sendDataTo(0, ctx.data_ptr, ctx.size, MemoryType::CPU);
-  if (status != status_t::SUCCESS) {
-    LOG(ERROR) << "[Channel " << ctx.id << "] Send failed.";
-  }
-}
-
-int main() {
+void UHMperformanceTestSendLogic(const std::string &server_ip) {
   FLAGS_colorlogtostderr = true;
   FLAGS_alsologtostderr = true;
 
-  const int channel_count = 1;
-  const std::string server_ip = "192.168.2.238";
-  const int base_port = 2025;
+  auto buffer = std::make_shared<ConnBuffer>(device_id, buffer_size);
+  Communicator* comm = new Communicator(buffer);
 
-  const int device_id = 1;
-  const size_t buffer_size = 2048ULL * 1024 * 16; // 32MB
+  Memory* mem = new Memory(0);
 
-  std::vector<std::shared_ptr<ConnBuffer>> buffers;
-  std::vector<Communicator*> communicators;
+  comm->initServer(server_ip, 2026, ConnType::RDMA);
+  comm->addNewRankAddr(0, server_ip, 12026);
+  comm->addNewRankAddr(1, server_ip, 2026);
+  comm->connectTo(0, ConnType::RDMA);
 
-  for (int i = 0; i < channel_count; ++i) {
-    auto buffer = std::make_shared<ConnBuffer>(device_id, buffer_size);
-    Communicator* comm = new Communicator(buffer);
-    comm->addNewRankAddr(0, server_ip, base_port + i);
-    comm->connectTo(0, ConnType::RDMA);
+  for (int power = 4; power <= 30; ++power) {
+    const size_t data_size = std::pow(2, power);
+    std::vector<uint8_t> host_data(data_size, 'A');
+    mem->allocateBuffer(&buffer->ptr, buffer_size);
+    mem->copyHostToDevice(buffer->ptr, host_data.data(), data_size);
+  
 
-    buffers.push_back(buffer);
-    communicators.push_back(comm);
-  }
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    // UHM interface
+    auto sendDataTo_start_time = high_resolution_clock::now();
 
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+    comm->sendDataTo(0, buffer->ptr, buffer_size, MemoryType::AMD_GPU);
 
-  for (int power = 25; power <= 30; ++power) {
-  const size_t total_size = std::pow(2, power);
-  std::vector<uint8_t> host_data(total_size, 'A');
-  size_t slice_size = total_size / channel_count;
+    auto sendDataTo_end_time = high_resolution_clock::now();
 
-  const int repeat = 3;
-  double total_MBps = 0.0;
+    auto sendDataTo_write_time = duration_cast<microseconds>(sendDataTo_end_time - sendDataTo_start_time);
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    // Serial interface
+    usleep(10000);
+    void *send1;
+    mem->allocateBuffer(&send1, data_size);
+    mem->copyHostToDevice(send1, host_data.data(), data_size);
+    
+    size_t buffer_size = buffer_size / 2;
+    size_t num_send_chunks =
+      (data_size + buffer_size - 1) / buffer_size;
 
-  for (int r = 0; r < repeat; ++r) {
-    std::vector<std::future<void>> futures;
-    auto start_time = std::chrono::high_resolution_clock::now();
+    long long total_time = 0;
+    char* send_buffer = (char*)send1;
 
-    for (int i = 0; i < channel_count; ++i) {
-      void* slice_ptr = host_data.data() + i * slice_size;
-      size_t actual_size = (i == channel_count - 1)
-                              ? total_size - i * slice_size
-                              : slice_size;
-
-      ChannelContext ctx = {
-          .id = i,
-          .comm = communicators[i],
-          .data_ptr = slice_ptr,
-          .size = actual_size};
-
-      futures.emplace_back(std::async(std::launch::async, send_channel_slice, ctx));
+    for(int i = 0; i < num_send_chunks; i++){
+      std::cout << "i = " << i << std::endl;
+      auto Serial_write_start_time = high_resolution_clock::now();
+      comm->sendDataTo(0, send_buffer + i * buffer_size, std::min(buffer_size, data_size - i * buffer_size), MemoryType::AMD_GPU);
+      auto Serial_write_end_time = high_resolution_clock::now();
+      usleep(10000);
+      total_time += duration_cast<microseconds>(
+            Serial_write_end_time - Serial_write_start_time).count();
     }
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
 
-    for (auto& f : futures) f.get();
-    auto end_time = std::chrono::high_resolution_clock::now();
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    // G2H2G interface
+    auto G2H2G_sendDataTo_start_time = high_resolution_clock::now();
+    void *sendG2H2G;
+    mem->allocateBuffer(&sendG2H2G, data_size);
+    mem->copyHostToDevice(sendG2H2G, host_data.data(), data_size);
 
-    std::chrono::duration<double> duration = end_time - start_time;
-    double seconds = duration.count();
+    comm->sendDataTo(0, sendG2H2G, buffer_size, MemoryType::AMD_GPU);
 
-    double throughput_MBps = (total_size / 1024.0 / 1024.0) / seconds;
-    double throughput_Gbps = throughput_MBps * 1024.0 * 1024.0 * 8 / 1e9;
+    auto G2H2G_end_time = high_resolution_clock::now();
 
-    total_MBps += throughput_MBps;
+    auto G2H2G_write_time = duration_cast<microseconds>(G2H2G_end_time - G2H2G_sendDataTo_start_time);
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    // Ideal RDMA interface
+    auto Ideal_sendDataTo_start_time = high_resolution_clock::now();
 
-    LOG(INFO) << "[Trial " << r + 1 << "] "
-              << total_size / (1024 * 1024) << " MB sent in "
-              << seconds << " s, "
-              << throughput_MBps << " MB/s ("
-              << throughput_Gbps << " Gbps)";
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-  }
+    comm->writeTo(0, 0, buffer_size);
 
-  double avg_MBps = total_MBps / repeat;
-  double avg_Gbps = avg_MBps * 1024.0 * 1024.0 * 8 / 1e9;
+    auto Ideal_sendDataTo_end_time = high_resolution_clock::now();
 
-  LOG(INFO) << ">>> Average over " << repeat << " trials: "
-            << avg_MBps << " MB/s ("
-            << avg_Gbps << " Gbps)";
-  LOG(INFO) << "--------------------------------------------";
-
-  std::this_thread::sleep_for(std::chrono::seconds(3));
+    auto Ideal_write_time = duration_cast<microseconds>(Ideal_sendDataTo_end_time - Ideal_sendDataTo_start_time); 
+    /*————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*/
+    string filename = "performanceTest.csv";
+      ofstream header_file(filename, ios::app);
+      if (header_file.tellp() == 0) {
+          header_file << "Data Size,Ideal,Serial,UHM,G2H2G\n";
+      }
+      header_file.close();
+      ofstream file(filename, ios::app); // Append mode
+      if (file.is_open()) {
+        file << power  << "," << Ideal_write_time.count() << "," << total_time <<"," << G2H2G_write_time.count() <<"\n";
+        file.close();
+        cout << "Data written to " << filename << endl;
+      } else {
+          cerr << "Failed to open file " << filename << endl;
+      }
+   }
 }
 
-  for (int i = 0; i < channel_count; ++i) {
-    communicators[i]->disConnect(0, ConnType::RDMA);
-    delete communicators[i];
-  }
+void performanceTest(std::vector<std::string> ips) {
+  for(std::string ip : ips) {
+      string filename = "performanceTest.csv";
+      ofstream header_file(filename, ios::app);
+      if (header_file.tellp() == 0) {
+          header_file << "Data Size,Ideal,Serial,UHM,G2H2G\n";
+      }
+      header_file.close();
+      ofstream file(filename, ios::app); // Append mode
+      if (file.is_open()) {
+          file<< ip << "\n";
+          file.close();
+          cout << "Data written to " << filename << endl;
+      } else {
+          cerr << "Failed to open file " << filename << endl;
+      }
+      UHMperformanceTestSendLogic(ip);
+    } 
+}
 
-  std::cout << "Client finished all transfers" << std::endl;
-  return 0;
+int main() {
+  std::string remote_ip_hygon1 = "192.168.2.240"; // hygon1
+  std::string remote_ip_hygon2 = "192.168.2.253"; // hygon2
+  std::string remote_ip_nv2 = "192.168.2.243"; // nv2
+  std::string remote_ip_nv3 = "192.168.2.247"; // nv3
+  std::string remote_ip_cam2 = "192.168.2.252"; // cam2
+  std::string remote_ip_moore = "192.168.2.238"; // moore
+
+  std::vector<std::string> ips;
+
+  ips.push_back(remote_ip_hygon1);
+  // ips.push_back(remote_ip_nv2);
+  // ips.push_back(remote_ip_cam2);
+
+  performanceTest(ips);
 }
